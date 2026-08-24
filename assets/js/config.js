87e98a97872b2f9ba31fb974d1adbad9f2e600d7
Arhj Sardar Hojaifa <arhjsardarhujaifa@gmail.com>
2026-08-24 07:44:39 +0600

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,17 @@
+const SITE_CONFIG = {
+  identity: {
+    name: "ARHJ Sardar Hojaifa",
+    title: "Travel Filmmaker • Explorer • Storyteller",
+    tagline: "Discovering the world through places, people, history, culture and stories."
+  },
+
+  social: {
+    youtube: "https://youtube.com/@ArhjSardarHojaifa",
+    facebookPage: "https://www.facebook.com/ArhjSardarHojaifa2",
+    facebookProfile: "https://www.facebook.com/ArhjSardarHojaifa",
+    tiktok: "https://www.tiktok.com/@arhjsardarhojaifa",
+    instagram: "https://www.instagram.com/arhjsardarhojaifa"
+  },
+
+  email: "thehojaifatravelexplorer@gmail.com"
+};
